4cc188a84a65fa56d9b371b6332a29218217172e
shaofeng li <2604566769@qq.com>
2023-11-07 12:07:22 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState,useEffect } from 'react';
 import './App.css';
 
 function App() {
+  let localCount = 0;
+  const storedCount = localStorage.getItem('count');
+  // function init(){
+    if (storedCount !== null) {
+      localCount = parseInt(storedCount);
+    } else {
+       localStorage.setItem('count', '0');
+    }
+  // }
+  // useEffect(() => {
+  //   console.log('useEffect');
+  //   init();
+  // });
+  const [count, setCount] = useState(localCount);
+  const add = () => {
+    console.log('add_1');
+    setCount(count + 1);
+    console.log('add_2');
+    localStorage.setItem('count', count.toString());
+  };
+  const sub= ()=>{
+    // localStorage.getItem('count');
+    setCount(count-1);
+    localStorage.setItem('count', count.toString());
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={add}>+</button>
+      <button onClick={sub}>-</button>
+      <p>count : {localCount} </p>
     </div>
   );
 }
